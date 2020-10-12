5a9a37799627993d611c03278a5dcc23b609dbea
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-12 20:00:27 -0300

    iniciando routes.

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/pages/landing.css';
+import logoImg from '../images/logo.svg';
+import { FiArrowRight } from 'react-icons/fi';
+
+function Landing() {
+    return (
+        <div id='page-landing'>
+        <div className='content-wrapper'>
+          <img src={logoImg} alt='Happy'/>
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+          <div className='location'>
+            <strong>São Leopoldo</strong>
+            <span>Rio Grande do Sul</span>
+          </div>
+          <a href='/app' className='enter-app'>
+            <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
+          </a>
+        </div>
+      </div>   
+    );
+}
+
+export default Landing;
---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import OrphanegasMap from './pages/OrphanagesMap';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Landing} />
+        <Route path='/app' component={OrphanegasMap} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;

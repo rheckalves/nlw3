823d0a29c44ffa7b0285a2942a49d41e82652480
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-14 12:17:10 -0300

    finalizado validacao de dados.

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
     async index (request: Request, response: Response) {
@@ -38,8 +39,8 @@ export default {
         const requestImages = request.files as Express.Multer.File[];
         const images = requestImages.map(image => {
             return { path: image.filename };
-        })
-        const orphanage = orphanagesRepository.create({
+        });
+        const data = {
             name,
             latitude,
             longitude,
@@ -48,7 +49,27 @@ export default {
             opening_hours,
             open_on_weekends,
             images,
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            opening_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                path: Yup.string().required(),
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const orphanage = orphanagesRepository.create(data);
 
         await orphanagesRepository.save(orphanage);
         return response.status(201).json(orphanage);

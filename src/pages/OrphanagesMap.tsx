00c1c283571fc11bbdfa261da7f4ca6038690876
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-14 18:07:56 -0300

    configurado api mapbox.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -33,7 +33,7 @@ function OrphanagesMap() {
         zoom={15}
         style={{ width: "100%", height: "100%" }}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
         <Marker icon={mapIcon} position={[-29.7806579, -51.1271811]}>
           <Popup
             closeButton={false}
